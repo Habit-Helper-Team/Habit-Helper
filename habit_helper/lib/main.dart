a12095c0bc32cf4ce072234d46f9b1dc985cd4d8
SfedBro <fegalinka@mail.ru>
2024-07-01 21:38:16 +0300

    dependencies

--- habit_helper/lib/main.dart
+++ habit_helper/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:habits_helper/screens/habits_screen.dart';
-import 'package:habits_helper/screens/home_screen.dart';
+import 'package:habit_helper/screens/habits_screen.dart';
+import 'package:habit_helper/screens/home_screen.dart';
 
 void main() => runApp(MaterialApp(
-  theme: ThemeData(
-    primaryColor: Colors.black45,
-  ),
-  home: Habits(),
-));
+      theme: ThemeData(
+        primaryColor: Colors.black45,
+      ),
+      home: Habits(),
+    ));
